50a7831a7288605a259f9cc0589d90176b50d51e
Pyetro <pyetroribeirosantacruz@gmail.com>
2025-08-14 22:41:56 -0300

    Trocando de jogador a cada turno

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -7,13 +7,26 @@ import chess.pieces.King;
 import chess.pieces.Rook;
 
 public class ChessMatch {
-
+	
+	private int turn;
+	private Color currentPlayer;	
     private Board board;
 
     public ChessMatch() {
         board = new Board(8, 8);
+        turn = 1;
+        currentPlayer = Color.WHITE;
         initialSetup();
     }
+    
+    public int getTurn() {
+    	return turn;
+    }
+    
+    public Color getCurrentePlaye() {
+    	return currentPlayer;
+    }
+    
 
     public ChessPiece[][] getPieces() {
         ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
@@ -38,6 +51,7 @@ public class ChessMatch {
         validateSourcePosition(source);
         validateTargetPosition(source,target);
         Piece capturedPiece = makeMove(source, target);
+        nextTurn();
         return (ChessPiece) capturedPiece;
     }
 
@@ -51,7 +65,11 @@ public class ChessMatch {
     private void validateSourcePosition(Position position) {
         if (!board.thereIsAPiece(position)) {
             throw new ChessExeption("There is no piece on source position");
+        } 
+        if(currentPlayer !=  ((ChessPiece)board.piece(position)).getColor()) {
+        	throw new ChessExeption("The chosen piece is not yours");
         }
+        
         if (!board.piece(position).inThereAnyPossibleMove()) {
 			throw new ChessExeption("There is no possible moves for the chosen pieces");
 		}
@@ -61,6 +79,11 @@ public class ChessMatch {
     	if(!board.piece(source).possibleMoves(target)) {
     		throw new ChessExeption("The chosen puece can't move to target position");
     	}
+    }
+    
+    private void nextTurn() {
+    	turn++;
+    	currentPlayer = (currentPlayer == Color.WHITE) ? Color.BLACK : Color.WHITE;
     }
 
     private void placeNewPiece(char column, int row, ChessPiece piece) {
